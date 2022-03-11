190e535b162b604b33b71134265a27aa07fbd706
Pankaj Kumar Rai <rai12091997@gmail.com>
2022-03-11 21:33:53 +0530

    add page title

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Head from 'next/head'
 import { Nav, MainSection, Card, BoostLink, CenterHeading, Footer } from '../Components'
 
 const BackgroundChange=styled.div`
@@ -8,6 +9,10 @@ const BackgroundChange=styled.div`
 export default function Home() {
   return (
     <>
+    <Head>
+        <title>Shortify-Link Shortner</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Nav />
       <MainSection />
       <BackgroundChange>
